e0c01ab6b74ddde07e38ccea80463f9976beec0a
Kevin Deldycke <kevin@deldycke.com>
2020-08-26 15:33:29 +0200

    Remove demo search indexes and styling.

--- plumage/static/js/application.js
+++ plumage/static/js/application.js
@@ -64,6 +64,16 @@
     $(".admonition.light").addClass('alert-light');
     $(".admonition.dark").addClass('alert-dark');
 
+    // Tipue Search results styling.
+    $("#tipue_search_results_count").addClass('text-muted small float-right');
+    $("#tipue_search_image_modal").addClass('d-none');
+    $(".tipue_search_result").addClass('border-bottom border-secondary mb-4 pb-3');
+    $(".tipue_search_content_title").addClass('h3');
+    $(".tipue_search_content_bold").addClass('bg-warning rounded px-1');
+    $(".tipue_search_content_url").addClass('d-none');
+    $(".tipue_search_image").addClass('float-left border rounded');
+    $(".tipue_search_note").addClass('d-none');
+
     // Allow videos to take the full width of a page
     $(".container").fitVids();
 
