d2a9c7b912eb009eff5e245ba20d89a1306c895e
Kevin Deldycke <kevin@deldycke.com>
2020-08-26 16:03:45 +0200

    Tweak URL search results style.

--- plumage/static/js/application.js
+++ plumage/static/js/application.js
@@ -70,7 +70,7 @@
     $(".tipue_search_result").addClass('border-bottom border-secondary mb-4 pb-3');
     $(".tipue_search_content_title").addClass('h3');
     $(".tipue_search_content_bold").addClass('bg-warning rounded px-1');
-    $(".tipue_search_content_url").addClass('d-none');
+    $(".tipue_search_content_url").addClass('small text-info');
     $(".tipue_search_image").addClass('float-left border rounded');
     $(".tipue_search_note").addClass('d-none');
 
