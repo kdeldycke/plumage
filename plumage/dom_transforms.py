fb000930185db767e5382cd4809eeb3ba523baa0
Kevin Deldycke <kevin@deldycke.com>
2023-06-20 01:03:28 +0400

    Rework headerlink

--- plumage/dom_transforms.py
+++ plumage/dom_transforms.py
@@ -30,10 +30,6 @@ def transform(path, context):
     doc("#content table").add_class("table table-hover")
     doc("#content table thead th").attr("scope", "col")
 
-    # Style TOC permalinks produced by Python's markdown.extensions.toc:
-    # https://python-markdown.github.io/extensions/toc/
-    doc(".headerlink").add_class("text-decoration-none small ps-2")
-
     # Make images responsive and styled in article content, but ignore
     # images in cards (like those from project template), and images attached to
     # links.
