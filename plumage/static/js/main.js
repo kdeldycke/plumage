eb1748249abb1a9b80b19f77c928085c34cc7e71
Kevin Deldycke <kevin@deldycke.com>
2024-07-22 15:16:23 +0400

    Fix JS

--- plumage/static/js/main.js
+++ plumage/static/js/main.js
@@ -8,18 +8,16 @@ var nav = document.querySelector(".navbar")
 var brand = document.querySelector(".navbar-brand")
 var navTop = nav.offsetTop
 var isFixed = 0
-
-
 window.onscroll = function (event) {
   if (window.scrollY >= navTop && !isFixed) {
     isFixed = 1
     nav.classList.add('fixed-top');
-    nav.classList.remove("rounded-3 d-block");
-    brand.classList.remove('d-none')
+    nav.classList.remove("rounded-3", "d-block");
+    brand.classList.remove('d-none');
   } else if (window.scrollY <= navTop && isFixed) {
     isFixed = 0
     nav.classList.remove('fixed-top');
-    nav.classList.add("rounded-3 d-block");
+    nav.classList.add("rounded-3", "d-block");
     brand.classList.add('d-none');
   }
 };
