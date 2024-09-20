e0dc3fcf13fff9553d6c1716b1f63c3553b3f23d
kdeldycke <159718+kdeldycke@users.noreply.github.com>
2024-09-20 08:54:29 +0000

    [changelog] Bump minor version to v5.1.0

--- plumage/__init__.py
+++ plumage/__init__.py
@@ -17,7 +17,7 @@
 import logging
 from pathlib import Path
 
-__version__ = "5.0.0"
+__version__ = "5.1.0"
 """ Examples of valid version strings according :pep:`440#version-scheme`:
 
 .. code-block:: python
